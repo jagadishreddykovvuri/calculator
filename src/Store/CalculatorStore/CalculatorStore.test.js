7df30fb7eb99c23ca277c70725733ec42f2f45ab
iB Admin <ib_admin@ib005.local>
2019-08-06 10:15:56 +0530

    [refactoring state] for the clear  expression in calculator store

--- src/Store/CalculatorStore/CalculatorStore.test.js
+++ src/Store/CalculatorStore/CalculatorStore.test.js
@@ -1,19 +1,16 @@
 import CalculatorStore from "./index";
 import { ensureExpectedIsNumber } from "jest-matcher-utils";
 describe("CalculatorStore", () => {
-  it("should able to set the evaluting expression", () => {
-    const calculatorStore = new CalculatorStore();
+  let calculatorStore;
+  beforeEach(() => {
+    calculatorStore = new CalculatorStore();
     calculatorStore.setExpression("10");
     calculatorStore.setExpression("+");
     calculatorStore.setExpression("20");
     expect(calculatorStore.expression).toBe("10+20");
   });
+  it("should able to set the evaluating expression", () => {});
   it("should Clear the value in expression when calling of clearExpression function", () => {
-    const calculatorStore = new CalculatorStore();
-    calculatorStore.setExpression("10");
-    calculatorStore.setExpression("+");
-    calculatorStore.setExpression("20");
-    expect(calculatorStore.expression).toBe("10+20");
     calculatorStore.clearExpression();
     expect(calculatorStore.expression).toBe("");
   });
