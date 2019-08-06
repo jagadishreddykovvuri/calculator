260175b6a2dc5982091a2f5ddaf55c103ce097ac
iB Admin <ib_admin@ib005.local>
2019-08-06 10:24:51 +0530

    [refactoring state] for deleting last character of the last expression

--- src/Store/CalculatorStore/CalculatorStore.test.js
+++ src/Store/CalculatorStore/CalculatorStore.test.js
@@ -7,14 +7,19 @@ describe("CalculatorStore", () => {
     calculatorStore.setExpression("10");
     calculatorStore.setExpression("+");
     calculatorStore.setExpression("20");
+  });
+  it("should able to set the evaluating expression", () => {
     expect(calculatorStore.expression).toBe("10+20");
   });
-  it("should able to set the evaluating expression", () => {});
   it("should Clear the value in expression when calling of clearExpression function", () => {
+    expect(calculatorStore.expression).toBe("10+20");
+
     calculatorStore.clearExpression();
     expect(calculatorStore.expression).toBe("");
   });
   it("should able to delete the last character of expression on calling Delete", () => {
+    expect(calculatorStore.expression).toBe("10+20");
+
     calculatorStore.deleteLastCharFromExpression();
     expect(calculatorStore.expression).toBe("10+2");
   });
